4940ec98f2baf41f0962fa9aac5631512fc22228
adityaastro <adityaastro@users.noreply.github.com>
2020-10-10 13:53:08 +0530

    Without Linked List

--- Data Structures/Week 1/Balanced Parenthesis.py
+++ Data Structures/Week 1/Balanced Parenthesis.py
@@ -1,107 +1,18 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-
-    def __str__(self):
-        return str(self.data)
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.size = 0
-
-    def append(self, *newNode):
-        nodes = []
-        self.size += len(newNode)
-        for entry in newNode:
-            nodes.append(Node(entry))
-        newNode = nodes
-        if self.head is None:
-            self.head = newNode[0]
-        lastNode = self.head
-        while lastNode.next:
-            lastNode = lastNode.next
-        for node in newNode:
-            lastNode.next = node
-            lastNode = node
-
-    def insert(self, node, pos):
-        self.size += 1
-        node = Node(node)
-        if not pos:
-            headnode = node
-            headnode.next = self.head
-            self.head = headnode
-        else:
-            currNode = self.head
-            i = 0
-            while currNode:
-                if i == pos - 1:
-                    break
-                currNode = currNode.next
-                i += 1
-            node.next = currNode.next
-            currNode.next = node
-
-    def __getitem__(self, pos):
-        currNode = self.head
-        i = 0
-        while currNode:
-            if i == pos:
-                return currNode.data
-            i += 1
-            currNode = currNode.next
-        raise ValueError('Index out of range')
-
-    def __len__(self):
-        return self.size
-
-    def delete(self, element):
-        i = 0
-        self.size -= 1
-        curNode = self.head
-        prevNode = Node()
-        while True:
-            if curNode.data == element:
-                break
-            prevNode = curNode
-            curNode = curNode.next
-        prevNode.next = curNode.next
-        if element == self.head.data:
-            prevNode = self.head.next
-            self.head = prevNode
-        del curNode
-
-    def __repr__(self):
-        p = ""
-        if self.head is None:
-            return "[]"
-        curNode = self.head
-        while curNode:
-            p += ", " + str(curNode)
-            curNode = curNode.next
-        return "[{}]".format(p[2:])
-
-
 class Stack:
     def __init__(self):
-        self.stack = LinkedList()
+        self.stack = list()
 
     def push(self, entry):
-        self.stack.insert(entry, 0)
+        self.stack.append(entry)
 
     def pop(self):
-        self.last = self.stack.head.data
-        self.stack.delete(self.last)
-        return self.last
+        return self.stack.pop()
 
     def __len__(self):
-        return self.stack.size
+        return len(self.stack)
 
     def peek(self):
-        return self.last
+        return self.stack[-1]
 
     def empty(self):
         return len(self.stack) == 0
