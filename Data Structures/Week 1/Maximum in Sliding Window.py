f94b17240941ae926349f0da74f19b4f4dcd6a21
adityaastro <adityaastro@users.noreply.github.com>
2020-10-12 15:10:35 +0530

    Maximum in Sliding Window

---
+++ Data Structures/Week 1/Maximum in Sliding Window.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+n = int(input())
+l = list(map(int, input().split()))
+m = int(input())
+q = deque()
+q.append(0)
+for i in range(m):
+    while q and l[i] >= l[q[-1]] :
+        q.pop()
+    q.append(i)
+
+for i in range(m, n):
+    print(l[q[0]], end = ' ')
+    if q[0] <= i - m:
+        q.popleft()
+    while q and l[i] >= l[q[-1]]:
+        q.pop()
+    q.append(i)
+
+print(l[q[0]])
