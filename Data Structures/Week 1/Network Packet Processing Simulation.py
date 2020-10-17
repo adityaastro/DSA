1b85b3baa6c1edbd12b2561a1800d88241be35bb
adityaastro <adityaastro@users.noreply.github.com>
2020-10-17 14:51:22 +0530

    All test cases passed

--- Data Structures/Week 1/Network Packet Processing Simulation.py
+++ Data Structures/Week 1/Network Packet Processing Simulation.py
@@ -1,22 +1,25 @@
 import sys
 from collections import deque
-
+# file = open('19.txt','r')
 Q = deque()
 S, N = map(int, sys.stdin.readline().split())
-if N == 0: quit()
-rem = S - 1
-Ans = [0] * N
+if N == 0:
+    quit()
+rem = S
+Ans = [-1] * N
 if S > 0:
+    rem -= 1
     Q.append(list(map(int, sys.stdin.readline().split())) + [0])
 else:
     for i in range(N):
         print(-1)
     quit()
 
-for i in range(N-1):
+for i in range(N - 1):
     currpacket = list(map(int, sys.stdin.readline().split()))
-    currpacket.append(i+1)
+    currpacket.append(i + 1)
     if len(Q) == 0:
+        rem -= 1
         Q.append(currpacket)
     else:
         while len(Q) > 0 and currpacket[0] >= Q[0][1]:
@@ -26,11 +29,11 @@ for i in range(N-1):
         if rem > 0:
             if len(Q) > 0:
                 currpacket[0] = Q[-1][1]
-                currpacket[1] += currpacket[0]
+            currpacket[1] += currpacket[0]
             Q.append(currpacket)
             rem -= 1
-        elif rem == 0:
-            Ans[currpacket[2]] = -1
+        # elif rem == 0:
+        #     Ans[currpacket[2]] = -1
 
 for q in Q:
     Ans[q[2]] = q[0]
